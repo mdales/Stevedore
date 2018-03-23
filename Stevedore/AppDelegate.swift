95bc7c3bf52bb92fc362fc27009ebd7b037c2f9a
Michael Dales <mdales@mac.com>
2018-03-23 13:21:36 +0000

    Initial Commit

---
+++ Stevedore/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Stevedore
+//
+//  Created by Michael Dales on 23/03/2018.
+//  Copyright © 2018 Digital Flapjack. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
